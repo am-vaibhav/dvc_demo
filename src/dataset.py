cfee3371958dbc14738ac7f6bc59b632d657dc0c
Vaibhav Kumar <vaibhav.kumar@diggrowth.com>
2026-08-12 03:34:10 +0530

    add first experiment

---
+++ src/dataset.py
@@ -0,0 +1,75 @@
+import numpy as np
+import pandas as pd
+import os
+import logging
+
+# logging configuration
+logger = logging.getLogger('data_ingestion')
+logger.setLevel('DEBUG')
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel('DEBUG')
+
+file_handler = logging.FileHandler('errors.log')
+file_handler.setLevel('ERROR')
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+
+def load_data(data_url: str) -> pd.DataFrame:
+    """Load data from a CSV file."""
+    try:
+        df = pd.read_csv(data_url)
+        logger.debug('Data loaded from %s', data_url)
+        return df
+    except pd.errors.ParserError as e:
+        logger.error('Failed to parse the CSV file: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error occurred while loading the data: %s', e)
+        raise
+
+
+def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
+    """Preprocess the data."""
+    try:
+        final_df = df[df["Length of Membership"] > 3]
+        return final_df
+    except KeyError as e:
+        logger.error('Missing column in the dataframe: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error during preprocessing: %s', e)
+        raise
+
+
+def save_data(df: pd.DataFrame, data_path: str) -> None:
+    """Save the train and test datasets."""
+    try:
+        raw_data_path = os.path.join(data_path, 'data_versions')
+        os.makedirs(raw_data_path, exist_ok=True)
+        df.to_csv(os.path.join(raw_data_path, "df_v1.csv"), index=False)
+    except Exception as e:
+        logger.error('Unexpected error occurred while saving the data: %s', e)
+        raise
+
+
+def main():
+    try:
+        df = load_data(data_url='https://raw.githubusercontent.com/araj2/customer-database/master/Ecommerce%20Customers.csv')
+        final_df = preprocess_data(df)
+        data_path = './data'
+        os.makedirs(data_path, exist_ok=True)
+        save_data(final_df, data_path)
+    except Exception as e:
+        logger.error('Failed to complete the data ingestion process: %s', e)
+        print(f"Error: {e}")
+
+
+if __name__ == '__main__':
+    main()
